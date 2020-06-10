71aa4382fae5643d77f71e2f7c9c61e5b021150c
trod <st104680@stud.uni-stuttgart.de>
2020-06-10 15:33:32 +0200

    SLADIM-3  Added GUI for Circuitbreaker configuration

--- priceserviceui/src/app/app.component.ts
+++ priceserviceui/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Breaker {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'priceserviceui';
+  selected = 'consecutive';
+  breakers: Breaker[] = [
+    {value: 'consecutive', viewValue: 'ConsecutiveBreaker'},
+    {value: 'sampling', viewValue: 'SamplingBreaker'}
+  ];
+  
+  isConsecutiveSelected: boolean = true;
+  selectInput(string: string) {
+    let selected = string;
+    if (selected == "consecutive") {
+      this.isConsecutiveSelected = true;
+    } else {
+      this.isConsecutiveSelected = false;
+    }
+  }
 }
--- priceserviceui/src/app/app.module.ts
+++ priceserviceui/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSelectModule } from "@angular/material/select";
+import { MatCardModule } from '@angular/material/card'; 
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from "@angular/flex-layout"; 
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatTooltipModule } from "@angular/material/tooltip"; 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
