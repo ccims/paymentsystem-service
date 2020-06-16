89ec27264909673b9b86788f2fd338f410602b0f
trod <st104680@stud.uni-stuttgart.de>
2020-06-16 13:11:22 +0200

    SLADIM-2 Added tooltips for breaker config

--- priceserviceui/src/app/app.module.ts
+++ priceserviceui/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-import { MatSelectModule } from "@angular/material/select";
-import { MatCardModule } from '@angular/material/card'; 
-import { MatButtonModule } from "@angular/material/button";
+import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
-import { FlexLayoutModule } from "@angular/flex-layout"; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { FormsModule } from "@angular/forms"; 
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
